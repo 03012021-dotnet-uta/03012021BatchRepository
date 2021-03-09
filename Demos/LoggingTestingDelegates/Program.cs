2aadf8d11618151ce32e74af39160b1a0575fcc3
Mark Moore <mark.moore@revature.com>
2021-03-08 22:14:48 -0600

    adds a simple console app logging example

---
+++ Demos/LoggingTestingDelegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingTestingDelegates
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var serviceCollection = new ServiceCollection();
+			ConfigureServices(serviceCollection);
+
+			using ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+			MathClass mc = serviceProvider.GetService<MathClass>();
+			mc.LoggingMethod();
+
+
+		}
+
+		private static void ConfigureServices(IServiceCollection services)
+		{
+			services.AddLogging(configure =>
+			{
+				configure.ClearProviders();
+				configure.AddConsole();
+				configure.SetMinimumLevel(LogLevel.Trace);
+			})
+			.AddTransient<MathClass>();
+		}
+	}
+}
