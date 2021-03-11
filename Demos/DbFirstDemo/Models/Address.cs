886908ad4ea043e4765dcd40b01a4281e9abdb20
Mark Moore <mark.moore@revature.com>
2021-03-11 14:19:32 -0600

    this addsd the Db First demo

---
+++ Demos/DbFirstDemo/Models/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DbFirstDemo.Models
+{
+    public partial class Address
+    {
+        public Address()
+        {
+            Customers = new HashSet<Customer>();
+        }
+
+        public int Addressid { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string CountryCode { get; set; }
+
+        public virtual ICollection<Customer> Customers { get; set; }
+    }
+}
---
+++ Demos/DbFirstDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DbFirstDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var dbContext = new Sample_DBTestContext())
+            {
+                List<Customer> customers = dbContext.Customers.Include(d => d.Address).ToList();
+                foreach (Customer c in customers)
+                {
+                    Console.WriteLine($"The customers name is {c.FirstName} {c.LastName}\n\tand his/her address is{c.AddressId}.");
+
+                }
+
+            }
+
+
+        }
+    }
+}
