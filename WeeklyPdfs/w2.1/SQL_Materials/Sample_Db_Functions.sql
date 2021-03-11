7f08fbe5227477f8dbe535019f1076ae4189c009
Mark Moore <mark.moore@revature.com>
2021-03-11 11:50:00 -0600

    adds SQL resources

---
+++ WeeklyPdfs/w2.1/SQL_Materials/Sample_Db_Functions.sql
@@ -0,0 +1,14 @@
+--- Scalar 
+CREATE FUNCTION dbo.GetNetSale(
+	@quantity INT,	
+	@unitprice dec(10,2),
+	@discount dec(10,2)
+)
+RETURNS dec(10,2)
+as 
+begin
+	return @quantity*@unitprice*(1-@discount);
+end
+
+-- call function (dbo means DataBase Owner)
+SELECT dbo.GetNetSale(10,100.00,0.1) as netSale;
