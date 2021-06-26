04568b72e5273748aa77b1a98a45c9cbfcdc3de6
Mark Moore <mark.moore@revature.com>
2021-06-26 14:02:52 -0500

    updates

--- Demos/RpsGame_RefactoredWithDb-testingInMemoryExample/RpsGameMain/RpsDbContext.cs
+++ Demos/RpsGame_RefactoredWithDb-testingInMemoryExample/RpsGameMain/RpsDbContext.cs
@@ -3,21 +3,21 @@ using System;
 
 namespace RpsGame_NoDb
 {
-    public class RpsDbContext : DbContext
-    {
-        public DbSet<Player> players { get; set; }
-        public DbSet<Round> rounds { get; set; }
-        public DbSet<Match> matches { get; set; }
+	public class RpsDbContext : DbContext
+	{
+		public DbSet<Player> players { get; set; }
+		public DbSet<Round> rounds { get; set; }
+		public DbSet<Match> matches { get; set; }
 
-        public RpsDbContext() { }
-        public RpsDbContext(DbContextOptions options) : base(options) { }
+		public RpsDbContext() { }
+		public RpsDbContext(DbContextOptions options) : base(options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            if (!options.IsConfigured)
-            {
-                options.UseSqlServer("Server=localhost\\SQLEXPRESS02;Database=RpsGame12142020;Trusted_Connection=True;");
-            }
-        }
-    }
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			if (!options.IsConfigured)
+			{
+				options.UseSqlServer("Server=localhost\\SQLEXPRESS02;Database=RpsGame12142020;Trusted_Connection=True;");
+			}
+		}
+	}
 }
